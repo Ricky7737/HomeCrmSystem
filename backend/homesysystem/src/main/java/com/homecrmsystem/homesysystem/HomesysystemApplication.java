20db1f7499fc1c71d5209ef69a935f417b8f24ce
Ricky7737 <baoqinghuang016@gmail.com>
2024-10-31 13:21:40 +0800

    Initial commit with backend and frontend

---
+++ backend/homesysystem/src/main/java/com/homecrmsystem/homesysystem/HomesysystemApplication.java
@@ -0,0 +1,13 @@
+package com.homecrmsystem.homesysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomesysystemApplication.class, args);
+	}
+
+}
